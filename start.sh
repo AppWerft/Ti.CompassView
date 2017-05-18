f1706bc5514d11843735bb49baefc1766d12031c
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-05-18 14:50:41 +0200

           

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 APPID=ti.compassview
-VERSION=1.0.0
+VERSION=0.0.2
 
 #cp android/assets/* iphone/
 
-JARFILE="~/Library/Application\ Support/Titanium/modules/android/$APPID/$VERSION/chromecast.jar"
 
 rm -rvf ~/Library/Application\ Support/Titanium/modules/android/$APPID/$VERSION/*
 
-cd android;rm -rf build/classes;ti --platform android build --build-only --sdk 6.0.3.GA; unzip -uo  dist/$APPID-android-$VERSION.zip  -d  ~/Library/Application\ Support/Titanium/; cd ..
+cd android;rm -rf build/classes;ti --platform android build --build-only --sdk 5.5.1.GA; unzip -uo  dist/$APPID-android-$VERSION.zip  -d  ~/Library/Application\ Support/Titanium/; cd ..
 #cd android;rm -rf build/classes;ant; unzip -uo  dist/$APPID-android-$VERSION.zip  -d  ~/Library/Application\ Support/Titanium/; cd ..
 
 
